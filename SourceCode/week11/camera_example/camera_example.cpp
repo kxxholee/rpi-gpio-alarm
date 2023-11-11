5b8f28f2136555b5791c979e46ddd6bb721e013f
kxxholee <kxxholee@gmail.com>
2023-11-11 20:31:00 +0900

    add cmake file

--- SourceCode/week11/camera_example/camera_example.cpp
+++ SourceCode/week11/camera_example/camera_example.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/photo.hpp>
 
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[]) {
+    VideoCapture cap;
+    cap.open("/dev/video0", CAP_V4L2);
+    if (!cap.isOpened()) {
+        printf("Can't open Camera\n");
+        return -1;
+    }
+    // Default resolutions of the frame are obtained. The default resolutions are system dependent.
+    int frame_width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    itn frame_height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+    VideoWriter video("outcpp.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, Size(frame_width, frame_height));
+
+    printf("Open Camera\n");
+    Mat img;
+    int count = 0;
+    int max;
+
+    if (argc > 1) {
+        max = int(argv[1]);
+    }
+    else {
+        max = 50;
+    }
+
+    while(count <= max) {
+        cap.read(img);
+        if (img.empty()) break;
+        video.write(img);
+        count++;
+    }
+
+    cap.release();
+    video.release();
+}
